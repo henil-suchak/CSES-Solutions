e4779a7b9769b2eb9d7ec593eb4a9cb49970e5be
henil suchak <henilsuchak8311@gmail.com>
2025-09-27 17:00:56 +0530

    solved elevator ride

--- Dynamic Programing/Elevator_Rides.cpp
+++ Dynamic Programing/Elevator_Rides.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
+
+int n;
+long long x;
+vector<long long> w;
+vector<pair<int,long long>> dp; 
+
+pair<int,long long> solve(int mask) {
+    if (mask == 0) return {1, 0}; 
+    if (dp[mask].first != -1) return dp[mask];
+
+    pair<int,long long> best = {n+1, 0}; 
+
+
+    for (int p = 0; p < n; p++) {
+        if (mask & (1 << p)) {
+            int prevMask = mask ^ (1 << p);
+            auto option = solve(prevMask);
+
+            if (option.second + w[p] <= x) {
+
+                option.second += w[p];
+            } else {
+
+                option.first += 1;
+                option.second = w[p];
+            }
+            best = min(best, option); 
+        }
+    }
+    return dp[mask] = best;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, maxWt;
-    cin >> n >> maxWt;
-    vector<int> personWt(n);
-    for (int i = 0; i < n; i++) cin >> personWt[i];
+    cin >> n >> x;
+    w.resize(n);
+    for (int i = 0; i < n; i++) cin >> w[i];
 
-    sort(personWt.begin(), personWt.end());
-
-    int ans = 0;
-    int left = 0, right = n - 1;
-
-    while (left <= right) {
-        if (personWt[left] + personWt[right] <= maxWt) {
-            left++;   // pair lightest
-            right--;  // with heaviest
-        } else {
-            right--;  // heaviest alone
-        }
-        ans++;
-    }
-
-    cout << ans << "\n";
+    dp.assign(1 << n, {-1, -1});
+    cout << solve((1 << n) - 1).first << "\n";
     return 0;
 }
